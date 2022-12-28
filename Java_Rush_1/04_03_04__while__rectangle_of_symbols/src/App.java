3f9faafd1b460431e4cc8879b85bc6f55d299735
Ruslan Lysenko <ruslanbek6@gmail.com>
2022-12-28 20:05:16 +0200

    Add task JR-1-4-3-4

---
+++ Java_Rush_1/04_03_04__while__rectangle_of_symbols/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        String sign = "Q";
+        int rows = 5;
+        int columns = 10;
+        int rows_ind = 0;
+        while (rows_ind < rows) {
+            int Columns_ind = 0;
+            while (Columns_ind < columns) {
+                System.out.print(sign);
+                Columns_ind++;
+            }
+            if (rows_ind != (rows - 1))
+                System.out.println();
+            rows_ind++;
+        }
+    }
+}

10dfa1bf773c84f3083375efe6b5c2a652ca14c8
Ruslan Lysenko <ruslanbek6@gmail.com>
2022-12-28 21:10:02 +0200

    Add task JR-1-4-5-2

---
+++ Java_Rush_1/04_05_02__while__max_of_entered_values/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        int num;
+
+        while (scan.hasNextInt()) {
+            num = scan.nextInt();
+            if ((num > max) && (num % 2 == 0)) {
+                max = num;
+            }
+        }
+        
+        scan.close();
+        
+        if (max != Integer.MIN_VALUE) {
+            System.out.print(max);
+        }
+    }
+}

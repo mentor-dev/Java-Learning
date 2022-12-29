f6691e5293f14afc6956d6d2ca84b9fa081ef753
Ruslan Lysenko <ruslanbek6@gmail.com>
2022-12-29 14:28:29 +0200

    Add task JR-1-4-5-3

---
+++ Java_Rush_1/04_05_03__while__processing_of_entered_numbers/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        int min = Integer.MAX_VALUE;
+        int min_2 = Integer.MAX_VALUE;
+        while (scan.hasNextInt()) {
+            int num = scan.nextInt();
+            if (num < min) {
+                min_2 = min;
+                min = num;
+            } else if (num < min_2 && num != min) {
+                min_2 = num;
+            }
+        }
+        scan.close();
+        System.out.print(min_2);
+    }
+}

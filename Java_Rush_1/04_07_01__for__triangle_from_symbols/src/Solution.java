a62ea3c251ec79c4cc2435d4133caeab85c55751
Ruslan Lysenko <ruslanbek6@gmail.com>
2022-12-30 20:33:09 +0200

    Add task JR-1-4-7-1

---
+++ Java_Rush_1/04_07_01__for__triangle_from_symbols/src/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        for (int row = 1; row <= 10; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+    }
+}

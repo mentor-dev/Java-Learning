de513894400b9705f57a6a4a395ed7c4b95872ec
Ruslan Lysenko <ruslanbek6@gmail.com>
2023-01-07 16:44:31 +0200

    Add task JR-1-8-4-1

---
+++ JavaRush_1/08_Data_types/04_01__objects_creation/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str1 = new String("Line 1");
+        String str2 = new String("Line 2");
+        String str3 = new String("Line 3");
+        String str4 = new String("Line 4");
+        String str5 = new String("Line 5");
+        String str6 = new String("Line 6");
+        String str7 = new String("Line 7");
+        String str8 = new String("Line 8");
+        String str9 = new String("Line 9");
+        String str10 = new String("Line 10");
+        int[] arr1 = new int[1];
+        int[] arr2 = new int[2];
+        int[] arr3 = new int[3];
+        int[] arr4 = new int[4];
+        int[] arr5 = new int[5];
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+    }
+}
